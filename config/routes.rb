e3204826948592336e6eaaa6c0103500653b3f21
jun.tutu <jun1_223@icloud.com>
2020-04-29 13:08:36 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'users/new'
+  get 'users/create'
+  get 'users/index'
+  get 'users/show'
   root to: 'tasks#index'
-  
   resources :tasks
+  
+  get 'signup', to: 'users#new'
+  resources :users, only: [:new, :create, :index, :show]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+fnames = %w[Taro Jiro Hana Jhon Mike]
+gnames = %w[Yamada Tanaka Suzuki Smith]
+
+(1..5).each do |num|
+  User.create(name: "#{fnames[num % 5]}#{gnames[num % 4]}", email: "#{fnames[num % 5]}@ta.com", password: "#{fnames[num % 5]}1234")
+end
