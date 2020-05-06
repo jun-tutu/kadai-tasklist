d476c2b6275a133eba94c87a41ab5eb29a046a8c
jun.tutu <jun1_223@icloud.com>
2020-05-06 04:07:41 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
-  get 'users/index'
-  get 'users/show'
   root to: 'tasks#index'
   resources :tasks
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
   resources :users, only: [:new, :create, :index, :show]
